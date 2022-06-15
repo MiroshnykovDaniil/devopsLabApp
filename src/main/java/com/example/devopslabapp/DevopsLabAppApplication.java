f1454713cc6aec7f9664556c17c69a614a4cf460
Daniil Miroshnykov <danielq911q@gmail.com>
2022-06-15 12:51:56 +0300

    initial

---
+++ src/main/java/com/example/devopslabapp/DevopsLabAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopslabapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsLabAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsLabAppApplication.class, args);
+    }
+
+}
