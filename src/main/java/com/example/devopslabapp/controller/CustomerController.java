eb3da951916be2dd41983549721040bcdfb4fc69
Daniil Miroshnykov <danielq911q@gmail.com>
2022-06-15 13:56:47 +0300

    add CustomerController

---
+++ src/main/java/com/example/devopslabapp/controller/CustomerController.java
@@ -0,0 +1,51 @@
+package com.example.devopslabapp.controller;
+
+
+import com.example.devopslabapp.model.Customer;
+import com.example.devopslabapp.service.CustomerService;
+import com.example.devopslabapp.service.CustomerServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController(value = "/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerServiceImpl customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(value = "/",produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<List<Customer>> getCustomers(){
+        return new ResponseEntity<>(customerService.getCustomers(),HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/",produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<Customer> getCustomer(@PathVariable String id){
+        return new ResponseEntity<>(customerService.getCustomerById(id),HttpStatus.OK);
+    }
+    @PostMapping(value = "/",produces = "application/json")
+    public ResponseEntity<Customer> insert(@RequestBody Customer customer){
+        return new ResponseEntity<>(customerService.insert(customer),HttpStatus.CREATED);
+    }
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable String id){
+        customerService.deleteCustomer(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<Customer> updateCustomer(@PathVariable String id, @RequestBody Customer customer){
+        customerService.updateCustomer(id,customer);
+        return new ResponseEntity<>(customerService.getCustomerById(id),HttpStatus.OK);
+    }
+
+}
