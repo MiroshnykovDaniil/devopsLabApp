b7793c216b4a22c99934368010504470cadce9da
Daniil Miroshnykov <danielq911q@gmail.com>
2022-06-15 14:28:30 +0300

    CustomerController return ResponseEntity

--- src/main/java/com/example/devopslabapp/controller/CustomerController.java
+++ src/main/java/com/example/devopslabapp/controller/CustomerController.java
@@ -5,15 +5,13 @@ import com.example.devopslabapp.model.Customer;
 import com.example.devopslabapp.service.CustomerService;
 import com.example.devopslabapp.service.CustomerServiceImpl;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
-import java.time.LocalDate;
 import java.util.List;
 
-@RestController(value = "/customers")
+@RestController()
+@RequestMapping(value = "/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -23,13 +21,11 @@ public class CustomerController {
     }
 
     @GetMapping(value = "/",produces = "application/json")
-    @ResponseBody
     public ResponseEntity<List<Customer>> getCustomers(){
         return new ResponseEntity<>(customerService.getCustomers(),HttpStatus.OK);
     }
 
-    @GetMapping(value = "/",produces = "application/json")
-    @ResponseBody
+    @GetMapping(value = "/{id}",produces = "application/json")
     public ResponseEntity<Customer> getCustomer(@PathVariable String id){
         return new ResponseEntity<>(customerService.getCustomerById(id),HttpStatus.OK);
     }
@@ -37,12 +33,13 @@ public class CustomerController {
     public ResponseEntity<Customer> insert(@RequestBody Customer customer){
         return new ResponseEntity<>(customerService.insert(customer),HttpStatus.CREATED);
     }
+    @DeleteMapping(value = "/{id}",produces = "application/json")
     public ResponseEntity<Customer> deleteCustomer(@PathVariable String id){
         customerService.deleteCustomer(id);
         return new ResponseEntity<>(HttpStatus.OK);
 
     }
-
+    @PutMapping(value = "/{id}",produces = "application/json")
     public ResponseEntity<Customer> updateCustomer(@PathVariable String id, @RequestBody Customer customer){
         customerService.updateCustomer(id,customer);
         return new ResponseEntity<>(customerService.getCustomerById(id),HttpStatus.OK);
